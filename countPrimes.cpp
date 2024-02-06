dce3795e1e26a70173e086b135e4711612bf5480
yiheng feng <yihengfeng@ucsb.edu>
2024-02-05 16:00:11 -0800

    add countPrimes.cpp to repo

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -5,5 +5,15 @@
 #include "utility.h"
 
 int countPrimes(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
-}
+  int count = 0;
+  for(int i = 0; i < size; i++){
+    if(a[i]> 2){
+      for(int j = 2; j < a[i]; j++)
+      if(a[i]%j != 0){
+          count += 1;
+      }
+    }
+  }
+  return count;
+  }; // STUB!  Replace with correct code.
+
